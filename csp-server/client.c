ad7f7c17a90af607679bfca6a28cc79704fdad33
Kasun <alankasun@gmail.com>
2018-02-17 00:26:27 +0530

    more

--- csp-server/client.c
+++ csp-server/client.c
@@ -49,12 +49,12 @@ int main(int argc, char**argv){
 	{
 		printf("Sentence %d : \n", i+1);
 		scanf("%s", sendline);
-		sendto(sockfd,sendline,strlen(sendline),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
-		n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+		//sendto(sockfd,sendline,strlen(sendline),0,(struct sockaddr*)&servaddr,sizeof(servaddr));
+		//n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
 		printf("%d\n", n);
-		recvline[n]=0;
+		//recvline[n]=0;
 
-		printf("%s\n", recvline);
+		printf("%s\n", "recvline");
 	}
 
 	
